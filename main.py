9d0250dd6db97f0da6923d5a75f929e4eea0a1da
Francesco0603 <162456739+Francesco0603@users.noreply.github.com>
2024-03-13 13:16:11 +0100

    Da finire

--- main.py
+++ main.py
@@ -15,15 +15,17 @@ while(True):
 
     if int(txtIn) == 1:
         print("Ok, quale parola devo aggiungere?")
-        txtIn = input()
-        t.handleAdd(txtIn.split(" "))
+        paroleInserite = input()
+        t.handleAdd(paroleInserite.split(" "))
         print("Aggiunta con successo!")
     if int(txtIn) == 2:
         print("Ok, quale parola devo tradurre?")
-        txtIn = input()
-        print(t.handleTranslate(txtIn))
+        paroleInserite = input()
+        print(t.handleTranslate(paroleInserite))
     if int(txtIn) == 3:
         print("Ok, quale parola devo cercare?")
         pass
     if int(txtIn) == 4:
+        print(t)
+    if int(txtIn) == 5:
         break
--- translator.py
+++ translator.py
@@ -10,42 +10,51 @@ class Translator:
         print("1. Aggiungi nuova parola")
         print("2. Cerca una traduzione")
         print("3. Cerca con wildcard")
-        print("4. Exit")
+        print("4. Stampa tutto il Dizionariog")
+        print("5. Exit")
         print("-----------------------------")
         print()
 
+    def __str__(self):
+        stampa = ""
+        for word in self.dizionario:
+            transl = ""
+            for tr in self.dizionario[word]:
+                transl += tr+","
+            stampa += word+" significa: "+transl[:len(transl)-1]+"\n"
+        return stampa
     def loadDictionary(self, dict):
         with open(dict,'r') as file:
             for line in file:
                 parole = line.split(" ")
                 self.dizionario[parole[0]] = [parole[1]]
 
-
     def handleAdd(self, entry):
         traduzioniNuove = []
         n = 1
         # salvo tutte le traduzioni inserite in input
-        while n <= len(entry):
+        while n < len(entry):
             traduzioniNuove.append(entry[n])
             n+=1
         # gestisco eventuali duplicazioni di parole
-        if self.dizionario[entry[0]].__eq__(traduzioniNuove):
-            raise ValueError("TRADUZIONI PRESENTI")
-        for traduzione in traduzioniNuove:
-            if traduzione not in self.dizionario[entry[0]]:
-                self.dizionario[entry[0]]
-
-
-
-        self.dizionario[entry[2]] = [entry[1]]
+        if self.dizionario.__contains__(entry[0]):
+            if self.dizionario[entry[0]] == (traduzioniNuove):
+                raise ValueError("TRADUZIONI PRESENTI")
+            for traduzione in self.dizionario[entry[0]]:
+                if traduzione not in traduzioniNuove:
+                    traduzioniNuove.append(traduzione)
+            self.dizionario[entry[0]] = traduzioniNuove
+        # se invece non c'è
+        else:
+            self.dizionario[entry[0]]= traduzioniNuove
 
     def handleTranslate(self, query):
-        traduzione = []
         for coppia in self.dizionario:
-            if coppia[0] == query:
-                traduzione.append(coppia[1])
-        return traduzione
-
+            if coppia == query:
+                trad = ""
+                for str in self.dizionario[coppia]:
+                    trad += str+", "
+                return trad[:len(trad)-2]
 
     def handleWildCard(self,query):
         # query is a string with a ? --> <par?la_aliena>
